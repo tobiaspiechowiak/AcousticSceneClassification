e5342aad361ab4a54b9a862f9a7c027ca8df5790
Tobias Piechowiak <tpiechowiak@gnresound.com>
2018-10-22 11:44:45 +0200

    Added more units

--- Matlab code/FeatureExtraction_testing.m
+++ Matlab code/FeatureExtraction_testing.m
@@ -16,12 +16,16 @@ for idxSubject = 1:nrSubjects
     
     %first subject
     ownVoice(1).mic(1).testing(idxSubject).mix = resample(audioread(strcat(path_external,'01-Front_left_',scene,'.wav')),fsDsp,fs,100);
-    ownVoice(1).mic(2).training(idxSubject).mix = resample(audioread(strcat(path_external,'02-Rear_left_',scene,'.wav')),fsDsp,fs,100);
-    ownVoice(1).mic(3).training(idxSubject).mix = resample(audioread(strcat(path_external,'03-MiE_left_',scene,'.wav')),fsDsp,fs,100);
+    ownVoice(1).mic(2).testing(idxSubject).mix = resample(audioread(strcat(path_external,'02-Rear_left_',scene,'.wav')),fsDsp,fs,100);
+    ownVoice(1).mic(3).testing(idxSubject).mix = resample(audioread(strcat(path_external,'03-MiE_left_',scene,'.wav')),fsDsp,fs,100);
     
-    ownVoice(2).mic(1).training(idxSubject).mix = resample(audioread(strcat(path_external,'04-Front_right_',scene,'.wav')),fsDsp,fs,100);
-    ownVoice(2).mic(2).training(idxSubject).mix = resample(audioread(strcat(path_external,'05-Rear_right_',scene,'.wav')),fsDsp,fs,100);
-    ownVoice(2).mic(3).training(idxSubject).mix = resample(audioread(strcat(path_external,'06-MiE_right_',scene,'.wav')),fsDsp,fs,100);               
+    ownVoice(2).mic(1).testing(idxSubject).mix = resample(audioread(strcat(path_external,'04-Front_right_',scene,'.wav')),fsDsp,fs,100);
+    ownVoice(2).mic(2).testing(idxSubject).mix = resample(audioread(strcat(path_external,'05-Rear_right_',scene,'.wav')),fsDsp,fs,100);
+    ownVoice(2).mic(3).testing(idxSubject).mix = resample(audioread(strcat(path_external,'06-MiE_right_',scene,'.wav')),fsDsp,fs,100);
+    
+    
+    refRMS(idxSubject,1) = rms(ownVoice(1).mic(1).testing(idxSubject).mix); %left front
+    refRMS(idxSubject,2) = rms(ownVoice(2).mic(1).testing(idxSubject).mix); %right front
     
 end
 
@@ -30,12 +34,36 @@ end
 for j = 1:nrSubjects %across subjects
     for i = 1:2
         for l = 1:3
-            ownVoice(i).mic(l).training(j).mix = 10^(refLevel/20)*ownVoice(i).mic(l).training(j).external/refRMS(j,1);           
+            ownVoice(i).mic(l).testing(j).mix = 10^(refLevel/20)*ownVoice(i).mic(l).training(j).external/refRMS(j,1);
         end
     end
 end
 
 %beamforming
+for i = 1:nrSubjects
+    %left
+    FD(1).ownVoice(i).mix = Two_Mic_BF(ownVoice(1).mic(1).testing(i).mix,...
+        ownVoice(1).mic(2).testing(i).mix,fsDSP);
+    
+    %right
+    FD(2).ownVoice(i).mix = Two_Mic_BF(ownVoice(2).mic(1).testing(i).mix,...
+        ownVoice(2).mic(2).testing(i).mix,fsDSP);
+    
+end
+
+
+
+
+
+%save shit
+disp('Save...')
+save('Scenes_testing.mat','ownVoice','FD');
+
+
+
+
+
+
 
 
 
--- Matlab code/VPU.m
+++ Matlab code/VPU.m
@@ -1,4 +1,6 @@
 
+function VPU()
+
 path = '../../Data/';
 fsDSP = 20833;
 blkSize = 32;
@@ -7,7 +9,7 @@ blkSize = 32;
 close all;
 
 blkFreq = floor(fsDSP / blkSize);
-winLength = round(0.010*blkFreq);
+winLength = round(0.010*blkFreq); %#ok<NASGU>
 
 %load the signals
 [Mix(1).data,fs] = audioread(strcat(path,'Voice+Cantine.wav'));
@@ -17,7 +19,7 @@ winLength = round(0.010*blkFreq);
 
 
 for idx = 1:3
-    Mix(idx).data = resample(Mix(idx).data,fsDSP,fs,50);   
+    Mix(idx).data = resample(Mix(idx).data,fsDSP,fs,50);
     for i = 1:length(Mix(idx).data)/blkSize - 1
         Mix(idx).blkmean(i,1) = mean(Mix(idx).data((i-1)*blkSize + 1 : i*blkSize,:));
     end
@@ -27,7 +29,7 @@ for idx = 1:3
     Mix(idx).t = linspace(0,floor(length(Mix(idx).blkmean))/blkFreq,floor(length(Mix(idx).blkmean)));
 end
 
-    
+
 f = linspace(0,fsDSP/2,512/2 +1);
 
 
@@ -71,8 +73,113 @@ xlabel('Time (sec)');
 colormap('parula')
 
 
+% amplitude distributions
+figure;
+histogram(Mix(1).blkmean);
+hold on;
+histogram(Mix(2).blkmean);
 
 
+% simple threshold criteria
+figure;
+plot(20*log10(abs(Mix(2).blkmean)))
+hold on; plot(20*log10(abs(Mix(1).blkmean)))
+hold on; plot(20*log10(abs(Mix(3).blkmean)))
+
+
+Mix(1).rms = 20*log10(rms(Mix(1).blkmean));
+Mix(2).rms = 20*log10(rms(Mix(2).blkmean));
+Mix(3).rms = 20*log10(rms(Mix(3).blkmean));
+
+
+%make peak & valley detection
+d = 1e-6*ones(length(Mix(1).blkmean),3);
+v = 1e-6*ones(length(Mix(1).blkmean),3);
+
+out = zeros(size(Mix(1).blkmean));
+
+for l = 1:3
+    for idx = 2 : blkFreq*50  %     length(Mix(1).blkmean)
+        
+        d(idx,l) =  Peak(Mix(l).blkmean(idx,1), d(idx - 1,l));
+        v(idx,l) =  Valley(Mix(l).blkmean(idx,1), v(idx - 1,l));
+        out(idx,l) = detect(d(idx,l),v(idx,l),35);
+        
+    end
+end
+
+
+
+figure;
+plot(20*log10(abs(Mix(1).blkmean)),'color',[0.8 0.8 0.8]);
+hold on; plot(20*log10(d),'r');
+hold on; plot(20*log10(v),'b');
+
+figure;
+subplot(2,1,1)
+plot(20*log10(abs(Mix(1).blkmean(6000:10000,1))),'color',[0.6 0.6 0.6]);
+hold on; plot(20*log10(d(6000:10000,1)),'r');
+hold on; plot(20*log10(v(6000:10000,1)),'b');
+
+subplot(2,1,2)
+plot(out(6000:10000,1),'b');
+set(gca,'YLim',[-1.3 1.3]);
+
+
+figure;
+subplot(2,2,1)
+plot(20*log10(abs(Mix(1).blkmean(6000:10000,1))),'color',[0.6 0.6 0.6]);
+hold on; plot(20*log10(d(6000:10000,1)),'r');
+hold on; plot(20*log10(v(6000:10000,1)),'b');
+
+subplot(2,2,3)
+plot(out(6000:10000,1),'b');
+set(gca,'YLim',[-1.3 1.3]);
+
+subplot(2,2,2)
+plot(20*log10(abs(Mix(2).blkmean(6000:10000,1))),'color',[0.6 0.6 0.6]);
+hold on; plot(20*log10(d(6000:10000,2)),'r');
+hold on; plot(20*log10(v(6000:10000,2)),'b');
+
+subplot(2,2,4)
+plot(out(6000:10000,2),'b');
+set(gca,'YLim',[-1.3 1.3]);
+
+
+
+
+    function d = Peak(x, d)
+        a = 0.4;
+        b = 150;
+        
+        if(abs(x) >= d)
+            d = a * d + (1-a)*abs(x);
+        else
+            d = (1 - 1/b) * d;
+        end
+    end
+
+
+    function v = Valley(x, v)
+        a = 0.95;
+        b = 400;
+        
+        if(abs(x) <= v)
+            v = a * v + (1-a)*abs(x);
+        else
+            v = (1 + 1/b) * v;
+        end
+    end
+
+
+
+    function out = detect(peak, valley, threshold)
+        
+        out = 20*log10(peak/valley) > threshold;
+        
+    end
+
+end
 
 
 
